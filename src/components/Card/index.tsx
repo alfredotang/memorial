54434a58f38981cf8c30549c921ac0858495d376
alfredotang <asdzxcca0617@gmail.com>
2021-02-14 01:19:15 +0800

    feat(card): card component add redirect function

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Avatar } from '@chakra-ui/react';
 import { motion, Variants } from 'framer-motion';
+import { PeopleListEntity } from '@src/data/people';
 
 const cardVariants: Variants = {
   hidden: { opacity: 1, scale: 0 },
@@ -10,12 +12,18 @@ const cardVariants: Variants = {
   },
 };
 
-type Props = {
-  src: string;
-  alt: string;
+type Props = Omit<PeopleListEntity, 'name'> & {
+  redirectTo?: string;
 };
 
-const Card: FC<Props> = ({ src, alt }) => {
+const Card: FC<Props> = ({ src, alt, redirectTo }) => {
+  const history = useHistory();
+  const handleRedirectToPeople = () => {
+    if (redirectTo) {
+      history.push(redirectTo);
+    }
+  };
+
   return (
     <motion.div
       initial="hidden"
@@ -23,7 +31,11 @@ const Card: FC<Props> = ({ src, alt }) => {
       variants={cardVariants}
       transition={{ duration: 1, delay: 1 }}
     >
-      <motion.div whileHover={{ scale: 1.5 }}>
+      <motion.div
+        whileHover={{ scale: 1.5 }}
+        whileTap={{ scale: 1.2 }}
+        onClick={handleRedirectToPeople}
+      >
         <Avatar src={src} name={alt} size="2xl" cursor="pointer" />
       </motion.div>
     </motion.div>
