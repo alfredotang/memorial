a92b27600d90a27212a411e100cccfc2281c992a
alfredotang <asdzxcca0617@gmail.com>
2021-02-14 03:12:25 +0800

    feat(layout): add [back to home]  button

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,8 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
+import { useHistory } from 'react-router-dom';
 import styled from '@emotion/styled';
 import { css } from '@emotion/react';
+import { Heading, Box, Button } from '@chakra-ui/react';
 import { theme } from '@src/theme';
 import { useMeasureScroll } from '@src/hooks';
 
@@ -20,17 +22,33 @@ const HeaderContent = styled.div<{ showShadow: boolean }>`
   position: fixed;
   top: 0px;
   align-items: center;
+  justify-content: space-between;
   display: flex;
   box-shadow: ${({ showShadow, theme }) =>
     showShadow ? theme.shadows['md'] : 'none'};
 `;
 
 const Header: FC = () => {
+  const history = useHistory();
   const { scrollY } = useMeasureScroll({ switchOn: true });
   const SHOW_HEADER_SHADOW = scrollY > 40;
+
+  const handleRedirectToHome = () => {
+    history.replace('/');
+  };
+
   return (
     <Wrapper>
-      <HeaderContent showShadow={SHOW_HEADER_SHADOW}>header</HeaderContent>
+      <HeaderContent showShadow={SHOW_HEADER_SHADOW}>
+        <Box display="flex" px={[10, 10, 20]}>
+          <Heading>Memorial</Heading>
+        </Box>
+        <Box display="flex" mr={[10, 20, 20]}>
+          <Button variant="link" onClick={handleRedirectToHome}>
+            Back To Home
+          </Button>
+        </Box>
+      </HeaderContent>
     </Wrapper>
   );
 };
