2d7b1c5a353a67a4254dea6dd22eb7bddf6b8a21
alfredotang <asdzxcca0617@gmail.com>
2021-02-14 01:23:22 +0800

    feat(layout): remove layout component testing css

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -4,7 +4,7 @@ import Header from './Header';
 
 const Layout: FC = ({ children }) => {
   return (
-    <div style={{ height: '200vh' }}>
+    <div>
       <Header />
       <main>{children}</main>
     </div>
