8f3dbf957b6c068ea913eff6d3bda8ef299d50c3
alfredotang <asdzxcca0617@gmail.com>
2021-02-14 23:42:24 +0800

    feat(people): fixed CardList

--- src/pages/People.tsx
+++ src/pages/People.tsx
@@ -1,7 +1,9 @@
 import { FC, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { Box } from '@chakra-ui/react';
 import { peopleListData } from '@src/data/people';
 import { CardList } from '@src/components';
+import { useMeasure } from '@src/hooks';
 
 function findPeople(name: string) {
   return peopleListData.find((item) => item.name === name);
@@ -10,12 +12,16 @@ function findPeople(name: string) {
 const People: FC = () => {
   const { name } = useParams<{ name: string }>();
   const people = findPeople(name);
+  const { ref, bounds } = useMeasure();
 
   return (
-    <Fragment>
-      <div>{people?.name || 'Not found'}</div>
-      <CardList duration={1} activeItem={name} enableActiveMode />
-    </Fragment>
+    <Box>
+      <Box mb={[5, 5, 10]} height={`${bounds.height}px`}>
+        <Box position="fixed" ref={ref}>
+          <CardList duration={1} activeItem={name} enableActiveMode />
+        </Box>
+      </Box>
+    </Box>
   );
 };
 
