0692eb63cd6d3976e28cdd5c1023d6630a878cc5
alfredotang <asdzxcca0617@gmail.com>
2021-02-15 19:06:37 +0800

    feat(MDProvider): add paragraph & <pre> & <code>

--- src/components/MDProvider/index.tsx
+++ src/components/MDProvider/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
-import { Code, Heading, Box } from '@chakra-ui/react';
+import { Code, Heading, Box, TypographyProps, Alert } from '@chakra-ui/react';
 import Markdown, { MarkdownToJSX } from 'markdown-to-jsx';
-import { useMarkdownQuery } from '@src/hooks';
+import { useMarkdownQuery, useRWD } from '@src/hooks';
 
 const H1: FC = ({ children }) => {
   return (
@@ -35,19 +35,37 @@ const H4: FC = ({ children }) => {
   );
 };
 
+type FontSizeVariants = TypographyProps['fontSize'];
+
+const Paragraph: FC = ({ children }) => {
+  const fontsize = useRWD<FontSizeVariants>(['md', 'xl', '2xl']);
+  return <Box fontSize={fontsize}>{children}</Box>;
+};
+
+const InlineCode: FC = ({ children }) => {
+  const fontsize = useRWD<FontSizeVariants>(['md', 'xl', '2xl']);
+  return <Code fontSize={fontsize}>{children}</Code>;
+};
+
+const Info: FC = ({ children }) => {
+  const fontsize = useRWD<FontSizeVariants>(['md', 'xl', '2xl']);
+  return <Alert fontSize={fontsize}>{children}</Alert>;
+};
+
 type Props = {
   src: string;
 };
+
 const MDProvider: FC<Props> = ({ src }) => {
   const { postMarkdown } = useMarkdownQuery(src);
   const overrides: MarkdownToJSX.Overrides = {
-    // "p" paragraph elements are substituted with MUI "Typography"
-    code: Code,
+    code: InlineCode,
     h1: H1,
     h2: H2,
     h3: H3,
     h4: H4,
-    p: Box,
+    p: Paragraph,
+    pre: Info,
   };
   return <Markdown options={{ overrides }}>{postMarkdown}</Markdown>;
 };
