f95a073f1b12f742d43fec69b7fd4fd0aca8e53b
alfredotang <asdzxcca0617@gmail.com>
2021-02-15 19:07:37 +0800

    feat(ProposeDay): fixed card list & add md

--- src/pages/ProposeDay/index.tsx
+++ src/pages/ProposeDay/index.tsx
@@ -1,8 +1,15 @@
-import { FC, Fragment } from 'react';
-import { SimpleGrid } from '@chakra-ui/react';
+import { FC } from 'react';
+import { SimpleGrid, Box, PositionProps } from '@chakra-ui/react';
 import { Banner, CardList } from '@src/components';
+import BannerImg from '@src/assets/images/banner/proposeDay.jpg';
+import ProposeDayMd from './proposeDay.md';
+import { useMeasure, useRWD } from '@src/hooks';
+
+type BottomVariants = PositionProps['bottom'];
 
 const ProposeDay: FC = () => {
+  const { ref, bounds } = useMeasure();
+  const bottom = useRWD<BottomVariants>(['15px', '15px', '30px']);
   return (
     <SimpleGrid
       column={2}
@@ -10,8 +17,12 @@ const ProposeDay: FC = () => {
       flexDirection="column"
       spacing={[5, 5, 10]}
     >
-      <Banner />
-      <CardList delay={1} duration={1} />
+      <Banner imgSrc={BannerImg} imgAlt="proposeDay" mdSrc={ProposeDayMd} />
+      <Box height={`${bounds.height}px`} display="flex" justifyContent="center">
+        <Box position="fixed" ref={ref} bottom={bottom}>
+          <CardList delay={1} duration={1} />
+        </Box>
+      </Box>
     </SimpleGrid>
   );
 };
